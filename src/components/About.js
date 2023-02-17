667b985beacfefb31d7b068ee86f48f3e6452bb3
Chris Henry <chrislouishenry@gmail.comgit statuscd~cd>
2023-02-17 13:13:19 -0600

    about section revamp

--- src/components/About.js
+++ src/components/About.js
@@ -10,27 +10,23 @@ function About () {
         <section className = "aboutSection">
             <section className="introduction-section">
                 <h1>Introduction</h1>
-                <p>I am a Developer!</p>
-                <p>I love creating and developing beautiful Apps with amazing design techniques.</p>
+                <p>Hello my name is CHRISTOPHER HENRY, and I am a DEVELOPER! I love creating and developing beautiful apps with amazing design techniques.</p>
+                <p></p>
             </section>
             <section className="location-section">
                 <h1>Where I'm From:</h1>
-                <p>I am from a suburb of St. Louis called Fenton </p>
+                <p>I am from St. Louis Missouri </p>
             </section>
             <section className="questions-section">
-                <h6> More About Me </h6>
                 <h2>What are your favorite hobbies?</h2>
-                <p>My favorite hobby is to play disc golf because it is a great free exercise, it takes a good amount of
-                    skill, and its extremely fun. </p>
-                <h2>What's your dream job?</h2>
-                <p>My dream job is similar to my current job except instead of problem-solving in the financial world, I
-                    would be doing it on the frontEnd of web development.</p>
-                <h2>Where do you live?</h2>
-                <p>I currently live in another suburb city outside of St. Louis, called Affton</p>
-                <h2>Why do you want to be a web developer?</h2>
+                <p>My favorite hobby is to play disc golf with friends because it is a great free exercise, and it takes a good amount of
+                    skill to play </p>
+                <h2>Purpose of this application?</h2>
+                <p>This app will help players to keep track of their Disc Golf Scores and Details so that they can continue to find more places to play, people to play with, and past scores to see how much they have improved each time they go out to play a game. </p>
+
+                <h2>Why did I want to become a web developer?</h2>
                 <p>Because programming is awesome and being able to make a difference in this world in a constantly
-                    changing environment makes more sense in my eyes. At my current job I am making a difference but
-                    they are small scale differences. Programming would be helping people on a much larger scale. </p>
+                    changing environment is very rewarding. I also love the design aspect of building something creative and hip for someone else to use. </p>
             </section>
         </section>
     )
